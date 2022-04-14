f0d1348ca0075f827e46de243d6b6bef093c00e4
Patricio Cárdenas <patricio.cardenas.palma@ciisa.cl>
2022-04-13 22:46:21 -0400

    Agrega código final

--- src/router.js
+++ src/router.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 const express = require('express')
 const InterestController = require('./controllers/InterestController')
 const PageController = require('./controllers/PageController')
@@ -10,8 +9,8 @@ const pageController = new PageController()
 const interestController = new InterestController()
 
 // Routes
-router.get('/', InterestController.renderInterestForm)
-router.post('/', InterestController.renderInterestResult)
+router.get('/', interestController.renderInterestForm)
+router.post('/', interestController.renderInterestResult)
 
 router.all('*', pageController.renderNotFound)
 
